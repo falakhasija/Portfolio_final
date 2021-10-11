7c35d54c07bcf8b7bf66a86839ec96eba014b739
falakhasija <49366890+falakhasija@users.noreply.github.com>
2021-10-11 23:51:15 +0530

    Add files via upload

---
+++ port.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".typing", 
+{
+    strings:[ "Developer", "Computer Science Graduate","Tech Enthusiast","Developer"],
+    typeSpeed:50,
+    backSpeed:20,
+    loop:true
+});
