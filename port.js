03dc6a012a5678d0c0ff184af16f7f87e300522b
falakhasija <49366890+falakhasija@users.noreply.github.com>
2021-10-23 15:11:20 +0530

    Update port.js

--- port.js
+++ port.js
@@ -83,4 +83,14 @@ function readMore() {
       moreText.style.display = "inline";
     }
   }
+var toastTrigger = document.getElementById('liveToastBtn')
+var toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger) {
+  toastTrigger.addEventListener('click', function () {
+    var toast = new bootstrap.Toast(toastLiveExample)
+
+    toast.show()
+  })
+}
+
  
